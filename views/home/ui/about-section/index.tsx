7daa13f4a316b9a92bb92c00f2a62e11a33e3143
andrey.kislov <andrey.kislov@chulakov.com>
2025-10-11 11:57:15 +0300

    feat: add adaptive for about-section

--- views/home/ui/about-section/index.tsx
+++ views/home/ui/about-section/index.tsx
@@ -1,49 +1,49 @@
-"use client";
+'use client';
 
-import { TimeLine } from "@/widgets/timeline";
-import css from "./about-section.module.css";
-import { RippleAnimation } from "@/shared/ui/ripple-animation";
+import { TimeLine } from '@/widgets/timeline';
+import css from './about-section.module.css';
+import { RippleAnimation } from '@/shared/ui/ripple-animation';
 import {
-  MARQUEE_IMAGES,
-  RANGE_CONTENT_HEADER,
-  RANGE_CONTENT_VALUE,
-  TIMELINE_DATA,
-} from "../../models/about-section.constants";
-import clsx from "clsx";
-import { useScroll } from "@/shared/hooks/use-scroll";
-import { useRef } from "react";
-import { getRangeContent } from "../../lib/get-range-content";
-import parser from "html-react-parser";
-import { Marquee } from "@/shared/ui/marcue";
+	MARQUEE_IMAGES,
+	RANGE_CONTENT_HEADER,
+	RANGE_CONTENT_VALUE,
+	TIMELINE_DATA,
+} from '../../models/about-section.constants';
+import clsx from 'clsx';
+import { useScroll } from '@/shared/hooks/use-scroll';
+import { useRef } from 'react';
+import { getRangeContent } from '../../lib/get-range-content';
+import parser from 'html-react-parser';
+import { Marque } from '@/shared/ui/marque';
 
 export const AboutSection = () => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const scrollPercentage = useScroll(containerRef);
+	const containerRef = useRef<HTMLDivElement>(null);
+	const scrollPercentage = useScroll(containerRef);
 
-  return (
-    <section className={css.root}>
-      <RippleAnimation />
-      <div className={clsx(css.wrapper, "container")} ref={containerRef}>
-        <TimeLine timelineData={TIMELINE_DATA} />
-        <h2 className={css.title}>
-          <span className={css.subtitle}>
-            {parser(getRangeContent(RANGE_CONTENT_HEADER, scrollPercentage))}
-          </span>
-          <span className={css.subtitle}>
-            {parser(getRangeContent(RANGE_CONTENT_VALUE, scrollPercentage))}
-          </span>
-        </h2>
-      </div>
-      <div className={clsx(css.skills)}>
-        <h2 className={clsx(css.marcueTitle, "container")}>Навыки</h2>
-        <Marquee speed={500} className={css.marcue} gap={10}>
-          {MARQUEE_IMAGES.map((image) => {
-            return (
-              <img className={css.image} key={image} src={image} alt="icon" />
-            );
-          })}
-        </Marquee>
-      </div>
-    </section>
-  );
+	return (
+		<section className={css.root}>
+			<RippleAnimation />
+			<div className={clsx(css.wrapper, 'container')} ref={containerRef}>
+				<TimeLine timelineData={TIMELINE_DATA} />
+				<h2 className={css.title}>
+					<span className={css.subtitle}>
+						{parser(getRangeContent(RANGE_CONTENT_HEADER, scrollPercentage))}
+					</span>
+					<span className={css.subtitle}>
+						{parser(getRangeContent(RANGE_CONTENT_VALUE, scrollPercentage))}
+					</span>
+				</h2>
+			</div>
+			<div className={clsx(css.skills)}>
+				<h2 className={clsx(css.marqueTitle, 'container')}>Навыки</h2>
+				<Marque speed={500} gap={10}>
+					{MARQUEE_IMAGES.map(image => {
+						return (
+							<img className={css.image} key={image} src={image} alt="icon" />
+						);
+					})}
+				</Marque>
+			</div>
+		</section>
+	);
 };
