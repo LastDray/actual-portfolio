74e35a9e3873cc03c6f7da0217ae501a114b1a14
andrey.kislov <andrey.kislov@chulakov.com>
2025-10-19 18:19:48 +0300

    fix: fix static path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
+const repo = 'actual-portfolio';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'export',
 	images: { unoptimized: true },
+	basePath: `/${repo}`,
+	assetPrefix: `/${repo}/`,
 };
 
 export default nextConfig;
